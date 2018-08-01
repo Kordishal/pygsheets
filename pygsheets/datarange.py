43441d3aaab9d8cb2ca82b6b791738425e979ddb
Kordishal <jonaswaeber@gmail.com>
2018-08-01 22:10:34 +0200

    Implemented Address class. Changed Datarange to use Address class as start and end.

--- pygsheets/datarange.py
+++ pygsheets/datarange.py
@@ -10,10 +10,86 @@ protected ranges, banned ranges etc.
 
 """
 
-import warnings
-
 from pygsheets.utils import format_addr
-from pygsheets.exceptions import InvalidArgumentValue, CellNotFound
+from pygsheets.exceptions import InvalidArgumentValue, CellNotFound, IncorrectCellLabel
+
+import warnings
+import re
+
+
+class Address(object):
+    """Represents the address of a cell.
+
+    >>> a = Address('A1')
+    >>> a.label
+    A1
+    >>> a[0]
+    1
+    >>> a[1]
+    1
+    >>> a = Address((1, 1))
+    >>> a.label
+    A1
+    """
+
+    _MAGIC_NUMBER = 64
+
+    def __init__(self, value):
+        if isinstance(value, str):
+            self._value = self._label_to_coordinates(value)
+        elif isinstance(value, tuple):
+            if value[0] < 1 or value[1] < 1:
+                raise InvalidArgumentValue('Address coordinates may not be below zero: ' + repr(self._value))
+            self._value = value
+        elif isinstance(value, Address):
+            self._value = self._label_to_coordinates(value.label)
+        else:
+            raise IncorrectCellLabel('Only labels in A1 notation or coordinates as a tuple are accepted.')
+
+    @property
+    def label(self):
+        return self._value_as_label()
+
+    def _value_as_label(self):
+        """Transforms tuple coordinates into a label of the form A1."""
+        row = int(self._value[0])
+        if row < 1:
+            raise InvalidArgumentValue('Address coordinates may not be below zero: ' + repr(self._value))
+        row_label = str(row)
+
+        col = int(self._value[1])
+        if col < 1:
+            raise InvalidArgumentValue('Address coordinates may not be below zero: ' + repr(self._value))
+        div = col
+        column_label = ''
+        while div:
+            (div, mod) = divmod(div, 26)
+            if mod == 0:
+                mod = 26
+                div -= 1
+            column_label = chr(mod + self._MAGIC_NUMBER) + column_label
+        return '{}{}'.format(column_label, row_label)
+
+    def _label_to_coordinates(self, label):
+        """Transforms a label in A1 notation into numeric coordinates and returns them as tuple."""
+        m = re.match(r'([A-Za-z]+)(\d+)', label)
+        if m:
+            column_label = m.group(1).upper()
+            row, col = int(m.group(2)), 0
+            for i, c in enumerate(reversed(column_label)):
+                col += (ord(c) - self._MAGIC_NUMBER) * (26 ** i)
+        else:
+            raise IncorrectCellLabel('Not a valid cell label format: {}.'.format(label))
+        return int(row), int(col)
+
+    def __iter__(self):
+        return self._value
+
+    def __getitem__(self, item):
+        return self._value[item]
+
+    def __eq__(self, other):
+        return self.label == other.label
 
 
 class DataRange(object):
@@ -43,11 +119,11 @@ class DataRange(object):
             end = (protectedjson['range'].get('endRowIndex', self._worksheet.cols),
                    protectedjson['range'].get('endColumnIndex', self._worksheet.rows))
             protect_id = protectedjson['protectedRangeId']
-        self._start_addr = format_addr(start, 'tuple')
-        self._end_addr = format_addr(end, 'tuple')
+        self._start_address = Address(start)
+        self._end_address = Address(end)
         if data:
-            if len(data) == self._end_addr[0] - self._start_addr[0] + 1 and \
-                            len(data[0]) == self._end_addr[1] - self._start_addr[1] + 1:
+            if len(data) == self._end_address[0] - self._start_address[0] + 1 and \
+                            len(data[0]) == self._end_address[1] - self._start_address[1] + 1:
                 self._data = data
             else:
                 self.fetch()
@@ -80,7 +156,7 @@ class DataRange(object):
         else:
             if self._name == '':
                 # @TODO handle when not linked (create an range on link)
-                self._worksheet.create_named_range(name, start=self._start_addr, end=self._end_addr)
+                self._worksheet.create_named_range(name, start=self._start_address, end=self._end_address)
                 self._name = name
             else:
                 self._name = name
@@ -112,29 +188,29 @@ class DataRange(object):
     @property
     def start_addr(self):
         """top-left address of the range"""
-        return self._start_addr
+        return self._start_address
 
     @start_addr.setter
     def start_addr(self, addr):
-        self._start_addr = format_addr(addr, 'tuple')
+        self._start_address = Address(addr)
         if self._linked:
             self.update_named_range()
 
     @property
     def end_addr(self):
         """bottom-right address of the range"""
-        return self._end_addr
+        return self._end_address
 
     @end_addr.setter
     def end_addr(self, addr):
-        self._end_addr = format_addr(addr, 'tuple')
+        self._end_address = Address(addr)
         if self._linked:
             self.update_named_range()
 
     @property
     def range(self):
         """Range in format A1:C5"""
-        return format_addr(self._start_addr) + ':' + format_addr(self._end_addr)
+        return '{}:{}'.format(self._start_address.label, self._end_address.label)
 
     @property
     def worksheet(self):
@@ -168,7 +244,7 @@ class DataRange(object):
         :param only_data: fetch only data
 
         """
-        self._data = self._worksheet.get_values(self._start_addr, self._end_addr, returnas='cells',
+        self._data = self._worksheet.get_values(self._start_address, self._end_address, returnas='cells',
                                                 include_tailing_empty_rows=True)
         if not only_data:
             pass
@@ -222,10 +298,10 @@ class DataRange(object):
     def _get_gridrange(self):
         return {
             "sheetId": self._worksheet.id,
-            "startRowIndex": self._start_addr[0]-1,
-            "endRowIndex": self._end_addr[0],
-            "startColumnIndex": self._start_addr[1]-1,
-            "endColumnIndex": self._end_addr[1],
+            "startRowIndex": self._start_address[0] - 1,
+            "endRowIndex": self._end_address[0],
+            "startColumnIndex": self._start_address[1] - 1,
+            "endColumnIndex": self._end_address[1],
         }
 
     def __getitem__(self, item):
